6d11da38a30d1cba98cc8c565685ac4d218191c3
Patrisha-de-Boon <32069658+Patrisha-de-Boon@users.noreply.github.com>
2018-06-27 13:38:38 -0600

    Added different boards, different backgrounds, and organised files

---
+++ Game/GameState.py
@@ -0,0 +1,152 @@
+import pygame  # load pygame keywords
+import sys     # let python use the file system
+import os      # help python identify the OS
+import Global  # includes variables meant to be common to multiple files
+
+# find all of the possible player moves, and show the necessary moves (ie. the capture moves)
+def findAllPlayerMoves(screen):
+    needToMove = []
+    if Global.PlayerTurn == 1:
+        PlayerPieces = Global.Player1List
+    elif Global.PlayerTurn == 2:
+        PlayerPieces = Global.Player2List
+    for piece in PlayerPieces: 
+        piece.findMoves(screen)
+        if piece.necessaryMoves:
+            needToMove.append(piece)
+    return needToMove
+    
+
+# completely redraw the entire screen and everything on it
+def redraw(screen, selectedPiece):
+    screen.blit(Global.boardImg, Global.boardImgRect)
+    if selectedPiece is not None:
+        selectedPiece.select(screen)
+    # draw the player pieces
+    for player in Global.Player1List:
+        player.draw(screen)
+    for player in Global.Player2List:
+        player.draw(screen)
+    pygame.display.update()
+    Global.toUpdate.clear()
+  
+# resize the screen and the items on it according to the user's manipulation of the screen size
+def resizeScreen(screen, width, height):
+    screen = pygame.display.set_mode((width, height), pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+    Global.Width = width
+    Global.Height = height
+    Global.drawBackground(screen)
+    if height <= width:
+        Global.boardImg = pygame.transform.scale(Global.get_image(os.path.join('Assets', 'Boards', 'Board' + str(Global.boardType) + '.png')), (int(9*height/10), int(9*height/10)))
+    else:
+        Global.boardImg = pygame.transform.scale(Global.get_image(os.path.join('Assets', 'Boards', 'Board' + str(Global.boardType) + '.png')), (int(9*width/10), int(9*width/10)))
+    Global.boardImgRect = Global.boardImg.get_rect()
+    Global.boardImgRect.center = (Global.Width/2, Global.Height/2)
+    for piece in Global.Player1List:
+        piece.resize(screen)
+    for piece in Global.Player2List:
+        piece.resize(screen)
+    
+
+# select and deselect player pieces according to user input
+def processMouseInput(screen, event, currPiece):
+    needToMove = findAllPlayerMoves(screen)
+    # if a piece is selected and either there are no necessary moves, or the selected piece must move
+    if currPiece is not None and (not needToMove or currPiece in needToMove):
+        for Object in currPiece.placeHolders.keys():
+            if Object.rect.collidepoint(event.pos):
+                x, y = currPiece.placeHolders[Object]
+                currPiece.move(screen, x, y)
+                necessaryMoves = []
+                if currPiece.isSelected:
+                    currPiece.findMoves(screen)
+                    necessaryMoves = currPiece.necessaryMoves.keys()
+                # if no moves are necessary, it is the next player's turn
+                if not necessaryMoves or not currPiece.isSelected:
+                    if Global.PlayerTurn == 1:
+                        Global.PlayerTurn = 2
+                    else:
+                        Global.PlayerTurn = 1
+                    currPiece.deselect(screen)
+                    return None
+                return currPiece
+
+    if Global.PlayerTurn == 1:    
+        for piece in Global.Player1List:
+            if piece.rect.collidepoint(event.pos):
+                if piece != currPiece:
+                    if currPiece is not None:
+                        currPiece.deselect(screen)
+                    if piece in needToMove or not needToMove:
+                        piece.findMoves(screen)
+                        piece.select(screen)
+                    else:
+                        piece.select(screen, canMove = False)
+                    return piece
+
+    elif Global.PlayerTurn == 2:
+        for piece in Global.Player2List:
+            if piece.rect.collidepoint(event.pos):
+                if piece != currPiece:
+                    if currPiece is not None:
+                        currPiece.deselect(screen)
+                    if piece in needToMove or not needToMove:
+                        piece.findMoves(screen)
+                        piece.select(screen)
+                    else:
+                        piece.select(screen, canMove = False)
+                    return piece
+
+    if currPiece is not None:
+        currPiece.deselect(screen)
+    return None
+
+# Run the Game
+def RunGame(screen):
+    Quit = False
+    selectedPiece = None
+    resizeScreen(screen, Global.Width, Global.Height)
+    redraw(screen, selectedPiece)
+
+    while not Quit:
+        redrawFlag = False
+        # handle player input
+        for event in pygame.event.get():
+            # quit the game if the user presses the x button on the window
+            if event.type == pygame.QUIT:
+                Quit = True
+                return 9
+
+            # enter the pause screen if the user presses escape in game
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                Quit = True
+                return 7
+
+            # resize the board and the pieces according to the user's manipulation of the screen size
+            elif event.type == pygame.VIDEORESIZE:
+                resizeScreen(screen, event.w, event.h)
+                redraw(screen, selectedPiece)
+            
+            # select and deselect pieces according to the user input
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                selectedPiece = processMouseInput(screen, event, selectedPiece)
+                redrawFlag = True
+                
+
+        # redraw the board and pieces when necessary
+        if redrawFlag:
+            pygame.display.update(Global.toUpdate)
+            Global.toUpdate.clear()
+        Global.clock.tick(Global.fps) # Limit fps of game
+
+        if not Global.Player1Dict or not Global.Player1Dict:
+            Quit = True
+            return 5
+
+
+
+
+
+
+
+        
---
+++ Game/Global.py
@@ -0,0 +1,83 @@
+import pygame  # load pygame keywords
+import os
+
+# attributes of user's display
+DisplayHeight = 0
+DisplayWidth = 0
+
+#attributes of game screen
+Height = 720
+Width = 1080
+
+WindowWidth = 500
+WindowHeight = 500
+boardType = 0  # default board type is 0 (the wooden one from this source http://www.vectorcopy.com/brown-wooden-chessboard-top-view-141-free-vector.html)
+checkerType = 0 # default board type is 0 (the wooden ones. The original unedited image came from here https://www.bearwood.com/product119.html)
+placeHolderType = 1 # default place holder type is 1 (edited version of checker type)
+PlayerTurn = 1
+backgroundType = 0
+fullscreen = pygame.FULLSCREEN
+
+# this is the list of all the main colours.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GOLD = (249, 166, 2)
+BLUE = (0, 0, 255)
+GRAY = (134, 136, 138)
+RED = (255, 0, 0)
+DARKBLUE = (16,24,115)
+
+fps = 60 # max fps of game (not really necessary here but good practice)
+clock = pygame.time.Clock() # game clock
+
+# sprite list for each player's pieces
+Player1List  = pygame.sprite.Group()
+Player2List  = pygame.sprite.Group()
+
+# dictionary to access location from player piece and vice versa
+Player1Dict = {}
+Player2Dict = {}
+
+# lists of rectangles that need to be drawn over
+toUpdate = []
+
+# the get_image function will load in the correct image for each enemy 
+# only once, and will store the image in a dictionary so that it doesn't have
+# to be loaded in pixel by pixel every time a new bullet is spawned. This code
+# came from this source https://nerdparadise.com/programming/pygame/part2.
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                image = pygame.image.load(path).convert_alpha()
+                _image_library[path] = image
+        return image
+
+boardImg = get_image(os.path.join('Assets', 'Boards', 'Board' + str(boardType) + '.png'))
+boardImgRect = boardImg.get_rect()
+boardImgRect.center = (Width/2, Height/2)
+
+backImg = get_image(os.path.join('Assets', 'Backgrounds', 'background' + str(backgroundType) + '.jpg'))
+
+def drawBackground(screen):
+        backImg = get_image(os.path.join('Assets', 'Backgrounds', 'background' + str(backgroundType) + '.jpg'))
+        rect = backImg.get_rect()
+        if rect.width > Width or rect.height > Height:
+                Ratio = rect.width/rect.height
+                if rect.width>Width:
+                        backImg = pygame.transform.scale(backImg, (Width, int(Width/Ratio)))
+                else:
+                        backImg = pygame.transform.scale(backImg, (int(Height*Ratio), Height))
+                rect = backImg.get_rect()
+        rect.left = 0
+        rect.top = 0
+        while rect.left <= Width:
+                while rect.top <= Height:
+                        screen.blit(backImg, rect)
+                        pygame.display.update(rect)
+                        rect.top += rect.height
+                rect.left += rect.width
+                rect.top = 0
+
+
